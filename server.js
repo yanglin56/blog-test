f7bba9f091d1dbf6817a8a232350b8cb54601d69
yanglin <yanglin556@yahoo.com>
2022-05-06 17:45:07 +0800

    修改下html内容

--- server.js
+++ server.js
@@ -24,12 +24,20 @@ var server = http.createServer(function(request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你好呀！`)
+        response.write(`
+        <!DOCTYPE html>
+        <head>
+        <link rel="stylesheet" href="/x">
+        </head>
+        <body>
+        <h1>你好呀</h1>
+        </body>
+        `);
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`h1{color: red;}`)
         response.end()
     } else {
         response.statusCode = 404
